5e9b465c5fbdc1f1333a54dc48122632ce4e5d06
Dev-J0 <dityx.obsidian@gmail.com>
2024-03-29 12:14:30 +0100

    going well

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ hamburger.onclick = function() {
     const closeButton = document.createElement('button');
 
     newDiv.classList.add('newDiv');
-    overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
     newDiv.textContent = 'This is menu';
-    newDiv.style.backgroundColor = 'white';
-    newDiv.style.height = '800px';
-    newDiv.style.width = '200px';
-    newDiv.style.position = 'fixed';
-    newDiv.style.right = '0';
-    newDiv.style.top = '0';
-    newDiv.style.zIndex = '2';
+
+    overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+    
+   
 
     const menu = document.getElementById('logo');
     menu.appendChild(newDiv);
@@ -22,7 +18,8 @@ hamburger.onclick = function() {
     closeButton.textContent = 'X'; 
     closeButton.classList.add('close-button');
     closeButton.onclick = function() { 
-        console.log('click');
+        newDiv.style.display = 'none';
+        overlay.style.display = 'none';
     };
 
     newDiv.appendChild(closeButton);
